bd57ba01227c503e459d1af861f707dae5d38fdf
teawon <xodnjsvkvk9@gmail.com>
2023-12-09 02:28:13 +0900

    feat : BaseballController 로직 구현

--- week1/javascript-baseball-6/src/Controller/BaseballController.js
+++ week1/javascript-baseball-6/src/Controller/BaseballController.js
@@ -22,31 +22,35 @@ class BaseballController {
       let isPlaying = true;
       while (isPlaying) {
         /// 사용자 입력을 받아 BaseBall 객체 생성
-        const playerInput = Console.readLineAsync("숫자를 입력해주세요 :");
+        const playerInput = await Console.readLineAsync(
+          "숫자를 입력해주세요 :"
+        );
         if (!playerInput) {
           throw new Error("[ERROR] 입력값이 없습니다.");
         }
         if (playerInput.length !== 3) {
           throw new Error("[ERROR] 3자리 숫자를 입력해주세요");
         }
-        const playerInputArr = playerInput.split("");
+        const playerInputArr = playerInput.split("").map(Number);
         const playerBaseBall = new BaseBall(playerInputArr);
 
         /// BaseBall 결과 비교
         const gameResult = new GameResult(playerBaseBall, computerBaseBall);
         const { ballCount, strikeCount } = gameResult.getResult();
+        console.log(ballCount, strikeCount);
         this.#printResult(ballCount, strikeCount);
+
         if (gameResult.checkIsGameEnd()) {
           isPlaying = false;
           Console.print("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
         }
-        // 재 시작 여부 확인
-        const userInput = Console.readLineAsync(
-          "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
-        );
-        if (userInput === "2") {
-          this.#isExit = true;
-        }
+      }
+      // 재 시작 여부 확인
+      const userInput = await Console.readLineAsync(
+        "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n"
+      );
+      if (userInput === "2") {
+        this.#isExit = true;
       }
     }
   }
@@ -61,15 +65,16 @@ class BaseballController {
     let result = "";
 
     if (ballCount === 0 && strikeCount === 0) {
-      Console.print("낫싱\n");
+      Console.print("낫싱");
+      return;
     }
     if (ballCount > 0) {
       result += `${ballCount}볼`;
     }
     if (strikeCount > 0) {
-      result += `${strikeCount}스트라이크`;
+      result += ` ${strikeCount}스트라이크`;
     }
-    Console.print(`${result}\n`);
+    Console.print(`${result.trim()}\n`);
   }
 }
 
