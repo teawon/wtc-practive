41f3ae04bbb6f9e895c4385c79a21cdb7c0801b0
teawon <xodnjsvkvk9@gmail.com>
2023-12-09 03:18:22 +0900

    refactor : controller 로직 함수 분리

--- week1/javascript-baseball-6/src/Controller/BaseballController.js
+++ week1/javascript-baseball-6/src/Controller/BaseballController.js
@@ -13,47 +13,62 @@ class BaseballController {
     this.computer = new Computer();
   }
   async start() {
-    // 초기 메세지 출력
     OutputView.printInit();
 
     while (!this.#isExit) {
-      // 초기값 세팅
-      const computerBaseBall = this.#getComputerAnswer();
+      await this.#playGame();
+    }
+  }
 
-      // 게임 진행 (맞출 때 까지 반복)
-      let isPlaying = true;
-      while (isPlaying) {
-        /// 사용자 입력을 받아 BaseBall 객체 생성
-        const playerInput = await InputView.readNumbers();
-        BaseBallValidator.baseBallNumberInput(playerInput);
+  async #playGame() {
+    const computerBaseBall = this.#getComputerAnswer();
 
-        const playerInputArr = playerInput.split("").map(Number);
-        const playerBaseBall = new BaseBall(playerInputArr);
+    let isPlaying = true;
+    while (isPlaying) {
+      const playerBaseBall = await this.#getPlayerBaseBall();
 
-        /// BaseBall 결과 비교
-        const gameResult = new GameResult(playerBaseBall, computerBaseBall);
-        const { ballCount, strikeCount } = gameResult.getResult();
+      const gameResult = new GameResult(playerBaseBall, computerBaseBall);
 
-        OutputView.printGameResult(ballCount, strikeCount);
+      const { ballCount, strikeCount } = gameResult.getResult();
+      OutputView.printGameResult(ballCount, strikeCount);
 
-        if (gameResult.checkIsGameEnd()) {
-          isPlaying = false;
-          OutputView.printCompleteGame();
-        }
-      }
-      // 재 시작 여부 확인
-      const userInput = await InputView.readExitOrContinue();
-      BaseBallValidator.exitOrContinueInput(userInput);
-      if (userInput === COMMAND.endGame) {
-        this.#isExit = true;
+      if (gameResult.checkIsGameEnd()) {
+        isPlaying = false;
+        OutputView.printCompleteGame();
       }
     }
+
+    await this.#checkExitOrContinue();
   }
 
   #getComputerAnswer() {
     const computerNumber = this.computer.createRandomNumber();
     const computerBaseBall = new BaseBall(computerNumber);
     return computerBaseBall;
+  }
+
+  async #checkExitOrContinue() {
+    const userInput = await this.#getContinueOrExitCommand();
+    if (userInput === COMMAND.endGame) {
+      this.#isExit = true;
+    }
+  }
+
+  async #getPlayerBaseBall() {
+    const playerInput = await InputView.readNumbers();
+    BaseBallValidator.baseBallNumberInput(playerInput);
+
+    const playerInputArr = playerInput.split("").map(Number);
+    const playerBaseBall = new BaseBall(playerInputArr);
+
+    return playerBaseBall;
+  }
+
+  async #getContinueOrExitCommand() {
+    const userInput = await InputView.readExitOrContinue();
+    BaseBallValidator.exitOrContinueInput(userInput);
+
+    return userInput;
   }
 }
 
