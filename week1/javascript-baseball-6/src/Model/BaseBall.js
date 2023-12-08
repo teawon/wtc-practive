75dce822e71a93e665347f83e3c4e06f1fcaae0d
teawon <xodnjsvkvk9@gmail.com>
2023-12-09 02:53:43 +0900

    refactor : BaseBall 도메인 예외처리 로직 분리

--- week1/javascript-baseball-6/src/Model/BaseBall.js
+++ week1/javascript-baseball-6/src/Model/BaseBall.js
@@ -1,23 +1,10 @@
+import BaseBallValidator from "../BaseBallValidator.js";
 class BaseBall {
   #number;
 
   constructor(number) {
-    this.#validate(number);
+    BaseBallValidator.baseBallValidator(number);
     this.#number = number;
-  }
-
-  #validate(number) {
-    if (number.length !== 3) {
-      throw Error("[ERROR] 3자리 숫자를 입력해주세요");
-    }
-
-    if (new Set(number).size !== 3) {
-      throw Error("[ERROR] 중복되지 않는 숫자를 입력해주세요");
-    }
-
-    if (number.every((num) => num < 0 || num > 9)) {
-      throw Error("[ERROR] 1부터 9까지의 숫자를 입력해주세요");
-    }
   }
 
   compareStrikeCount(other) {
