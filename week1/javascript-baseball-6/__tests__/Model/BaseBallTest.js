c592d2ea7db6aeeb40cf38613c96716ae3c04e2f
teawon <xodnjsvkvk9@gmail.com>
2023-12-09 03:27:05 +0900

    test : BaseBall 예외처리 테스트 코드 추가

--- week1/javascript-baseball-6/__tests__/Model/BaseBallTest.js
+++ week1/javascript-baseball-6/__tests__/Model/BaseBallTest.js
@@ -12,6 +12,27 @@ describe("BaseBall 모델 테스트", () => {
     expect(baseBall.getNumber()).toEqual(number);
   });
 
+  test("유효하지 않은 입력을 받으면 에러가 발생한다", () => {
+    //given
+    const invalidLenNum = [1, 2, 3, 4];
+    const duplicatedNum = [1, 1, 1];
+    const invalidRengeNum = [-1, 2, 20];
+
+    //when
+    const invalidLenAction = () => new BaseBall(invalidLenNum);
+    const duplicatedAction = () => new BaseBall(duplicatedNum);
+    const invalidRengeAction = () => new BaseBall(invalidRengeNum);
+
+    //then
+    expect(invalidLenAction).toThrow("[ERROR] 3자리 숫자를 입력해주세요");
+    expect(duplicatedAction).toThrow(
+      "[ERROR] 중복되지 않는 숫자를 입력해주세요"
+    );
+    expect(invalidRengeAction).toThrow(
+      "[ERROR] 1부터 9까지의 숫자를 입력해주세요"
+    );
+  });
+
   test("두 BaseBall을 비교해 자리수와 값이 일치하는 스트라이크 개수를 반환한다", () => {
     //given
     const src = [1, 2, 3];
